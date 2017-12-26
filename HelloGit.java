fa90e4acb965690c6df47c149f70c319c68cb775
SuperYG1991 <625029060@qq.com>
2017-12-27 01:00:13 +0800

    Modified javafile

--- HelloGit.java
+++ HelloGit.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 public class  HelloGit{
 	public static void main(String[] args){
-		String a = "hello world";	
 		Random r = new Random();
 		int num = r.nextInt(10);
 		if(num==1){
-			System.out.println(a+"恭喜您中奖了");
+			System.out.println("恭喜您中奖了");
 		}else{
 			System.out.println("抽中的数字为：" + num);
 			System.out.println("谢谢参与，继续加油");
